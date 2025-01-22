d72bcf20fc88e4c9e651302ee4df08655c3364fc
huyphan67 <huypdn6703@gmail.com>
2025-01-22 21:31:49 +0700

    video 29

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyComponent from './components/learn/MyComponent'
+import SecondComponent from './components/learn/SecondComponent'
 
-const App = () =>  {
+const App = () => {
   const [count, setCount] = useState(0)
 
-  const myFunction = () => {
-    console.log(">> run my arrow function")
-  }
-
-  myFunction();
-  
   return (
     <>
       <div>
@@ -23,6 +19,8 @@ const App = () =>  {
         </a>
       </div>
       <h1>Hello world</h1>
+      <MyComponent />
+      <SecondComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,7 @@
+const MyComponent = () => {
+    return (
+        <div>hello my name's Huy</div>
+    );
+}
+
+export default MyComponent;
