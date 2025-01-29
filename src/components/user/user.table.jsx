052aa820d10cf7cdc19ff174fe5330b5b0a7c6bb
huyphan67 <huypdn6703@gmail.com>
2025-01-29 11:13:09 +0700

    video 77

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,11 +1,24 @@
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import { Table } from "antd";
+import UpdateUserModal from "./update.user.modal";
+import { useState } from "react";
 
 const UserTable = (props) => {
     const { dataUsers } = props;
+
+    const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+
+    const [dataUpdate, setDataUpdate] = useState(null);
+
     const columns = [
         {
             title: 'Id',
             dataIndex: '_id',
+            render: (_, record) => {
+                return (
+                    <a href='#'>{record._id}</a>
+                )
+            }
         },
         {
             title: 'Full Name',
@@ -15,14 +28,38 @@ const UserTable = (props) => {
             title: 'Email',
             dataIndex: 'email',
         },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <div style={{ display: "flex", gap: "20px" }}>
+                    <EditOutlined
+                        onClick={() => {
+                            setDataUpdate(record);
+                            setIsModalUpdateOpen(true);
+                        }}
+                        style={{ cursor: "pointer", color: "orange" }} />
+                    <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+                </div>
+            ),
+        },
     ];
 
     return (
-        <div>
-            <Table dataSource={dataUsers} columns={columns} rowKey={"_id"} />
-        </div>
+        <>
+            <Table
+                columns={columns}
+                dataSource={dataUsers}
+                rowKey={"_id"}
+            />
+            <UpdateUserModal
+                isModalUpdateOpen={isModalUpdateOpen}
+                setIsModalUpdateOpen={setIsModalUpdateOpen}
+                dataUpdate={dataUpdate}
+                setDataUpdate={setDataUpdate}
+            />
+        </>
     )
-
 }
 
 export default UserTable;
