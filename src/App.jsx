8fa482f08c7d0e9c8b75e0b0ec1fc35290eafe17
huyphan67 <huypdn6703@gmail.com>
2025-01-29 22:51:52 +0700

    video 81

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import TodoData from "./components/todo/TodoData"
-import TodoNew from "./components/todo/TodoNew"
 import './components/todo/todo.css';
-import reactLogo from './assets/react.svg';
-import { useState } from "react";
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
 import { Outlet } from "react-router-dom";
